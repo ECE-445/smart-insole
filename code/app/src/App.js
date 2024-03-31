b2101f1042fc36bd783d4f207b78710c0abf0e6a
Ramsey <ramvandermeer@gmail.com>
2024-03-31 18:28:30 -0500

    Added chart to main page

--- code/app/src/App.js
+++ code/app/src/App.js
@@ -1,4 +1,5 @@
-import BasicHeatMap from "./components/HeatMap";
+import BasicHeatMap from "./components/HeatMap"
+import ChartComponent from "./components/ChartComponent.js"
 
 function App() {
   return (
@@ -6,12 +7,19 @@ function App() {
       <header>
         <h1>Smart Analytics Insole</h1>
       </header>
-      <BasicHeatMap 
-        width={800} 
-        height={400} 
-        events={true} 
-        margin={{ top: 20, left: 30, right: 30, bottom: 50 }} 
-        separation={30} 
+      <BasicHeatMap
+        width={800}
+        height={400}
+        events={true}
+        margin={{ top: 20, left: 30, right: 30, bottom: 50 }}
+        separation={30}
+      />
+      <ChartComponent
+        data={Array.from({ length: 100 }, (_, i) =>
+          Math.floor(Math.random() * i * 10)
+        )}
+        lables={Array.from({ length: 100 }, (_, i) => i + 1)}
+        title="Chart Title"
       />
     </div>
   )
